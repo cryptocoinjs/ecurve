38cc85f7be7222e6719d5a165ffd3c33b68235ed
JP Richardson <jprichardson@gmail.com>
2014-05-29 20:38:40 -0500

    tests for names.test.js fixed

--- test/names.test.js
+++ test/names.test.js
@@ -10,7 +10,7 @@ describe('+ getSECCurveByName(curveName)', function() {
       T (curve);
       EQ (curve.getCurve().getQ().toBuffer().toString('hex'), 'fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f');
       T (curve.getCurve().getA().toBigInteger().equals(BigInteger.ZERO));
-      EQ (arrayToHex(curve.getCurve().getB().toBigInteger().toByteArrayUnsigned()), '07');
+      EQ (curve.getCurve().getB().toBigInteger().toBuffer().toString('hex'), '07');
       EQ (new Buffer(curve.getG().getEncoded(false)).toString('hex'), '0479be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8');
       EQ (curve.getN().toBuffer().toString('hex'), 'fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141');
       EQ (curve.getH().toBuffer().toString('hex'), '01');
