0a6230128e1a722ec93648c7c1c9dd893cb41213
JP Richardson <jprichardson@gmail.com>
2014-05-31 16:12:19 -0500

    tests to fetch all curves

--- test/names.test.js
+++ test/names.test.js
@@ -3,7 +3,7 @@ var getECParams = require('../').getECParams
 
 require('terst')
 
-var curves = ['secp128r1']
+var curves = ['secp128r1', 'secp160k1', 'secp160r1', 'secp192k1', 'secp192r1', 'secp224r1', 'secp256k1', 'secp256r1']
 
 describe('+ getECParams(curveName)', function() {
   describe('> when the bitcoin curve is passed', function() {
