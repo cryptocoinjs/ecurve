9085e2fe21f4234ce078119fa8368c29dc4463bd
JP Richardson <jprichardson@gmail.com>
2014-05-31 16:36:44 -0500

    screwed up on property casing.

--- test/names.test.js
+++ test/names.test.js
@@ -13,9 +13,9 @@ describe('+ getECParams(curveName)', function() {
       EQ (ecparams.curve.q.toBuffer().toString('hex'), 'fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f');
       T (ecparams.curve.a.toBigInteger().equals(BigInteger.ZERO));
       EQ (ecparams.curve.b.toBigInteger().toBuffer().toString('hex'), '07');
-      EQ (new Buffer(ecparams.G.getEncoded(false)).toString('hex'), '0479be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8');
-      EQ (ecparams.N.toBuffer().toString('hex'), 'fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141');
-      EQ (ecparams.H.toBuffer().toString('hex'), '01');
+      EQ (new Buffer(ecparams.g.getEncoded(false)).toString('hex'), '0479be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8');
+      EQ (ecparams.n.toBuffer().toString('hex'), 'fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141');
+      EQ (ecparams.h.toBuffer().toString('hex'), '01');
     });
   });
 
