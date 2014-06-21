c38376e8032dc4e86318499fc29aee51a3e474c7
Daniel Cousens <github@dcousens.com>
2014-06-21 22:53:10 +1000

    names: test all curves

--- test/names.test.js
+++ test/names.test.js
@@ -1,35 +1,32 @@
 var assert = require('assert')
-var BigInteger = require('bigi')
-
+var ecurve = require('../')
 var getCurveByName = require('../lib/names')
 
-var curves = ['secp128r1', 'secp160k1', 'secp160r1', 'secp192k1', 'secp192r1', 'secp224r1', 'secp256k1', 'secp256r1']
+var BigInteger = require('bigi')
 
-describe('+ getCurveByName(curveName)', function() {
-  describe('> when the secp256k1 curve is passed', function() {
-    it('should return the proper curve', function() {
-      var curve = getCurveByName('secp256k1')
-      assert(curve)
-      assert.equal(curve.p.toBuffer().toString('hex'), 'fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f')
-      assert.equal(curve.a.toBuffer().toString('hex'), '') // 0 becomes ''
-      assert.equal(curve.b.toBuffer().toString('hex'), '07')
-      assert.equal(curve.G.getEncoded(false).toString('hex'), '0479be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8')
-      assert.equal(curve.n.toBuffer().toString('hex'), 'fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141')
-      assert.equal(curve.h.toBuffer().toString('hex'), '01')
-    })
-  })
+var curves = require('../lib/curves')
 
-  curves.forEach(function(c) {
-    describe('> when ' + c, function() {
+describe('getCurveByName(curveName)', function() {
+  for (var curveName in curves) {(function(curveName) {
+    var curveData = curves[curveName]
+
+    describe('when given ' + curveName, function() {
       it('should return the curve', function() {
-        var ecparams = getCurveByName(c)
-        assert(ecparams)
+        var curve = getCurveByName(curveName)
+
+        assert.equal(curve.p.toHex(), curveData.p)
+        assert.equal(curve.a.toHex() || "00", curveData.a)
+        assert.equal(curve.b.toHex(), curveData.b)
+        assert.equal(curve.G.affineX.toHex(), curveData.Gx)
+        assert.equal(curve.G.affineY.toHex(), curveData.Gy)
+        assert.equal(curve.n.toHex(), curveData.n)
+        assert.equal(curve.h.toHex(), curveData.h)
       })
     })
-  })
+  })(curveName)}
 
-  describe('> when null is passed', function() {
-    it('should return null for unknown curves', function() {
+  describe('when an unknown curve is requested', function() {
+    it('should return null', function() {
       assert.equal(getCurveByName('foobar'), null)
     })
   })
